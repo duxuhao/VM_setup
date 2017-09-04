c674b2bebc358ef97564c71bd4228b97d530c882
duxuhao <duxuhao88@gmail.com>
2017-09-04 01:37:03 +0000

    deploy environment

---
+++ setup.sh
@@ -0,0 +1,29 @@
+apt-get install python3-setuptools python3-dev build-essential
+
+apt-get install python3-pip python3-dev
+
+pip3 install --upgrade pip
+
+pip3 install --upgrade virtualenv
+
+pip3 install numpy
+
+pip3 install scipy
+
+pip3 install matplotlib
+
+pip3 install pandas
+
+pip3 install sklearn
+
+pip3 install xgboost
+
+pip3 install tensorflow
+
+pip3 install seaborn
+
+pip3 install wavio
+
+pip3 install adaptfilt
+
+pip3 install PyWavelets
